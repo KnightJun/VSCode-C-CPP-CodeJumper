534d1704588d056a7a494c7f9372567a7de9ed0e
knightjun <None>
2019-12-16 23:23:53 +0800

    加入vscode定义与引用接口

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { basename,extname, dirname, relative,resolve, normalize} from 'path';
-import { existsSync } from 'fs';
+import { existsSync, futimes } from 'fs';
+import { execSync, exec, ExecException,ExecSyncOptionsWithStringEncoding } from 'child_process';
+
+enum GtagsFindType{
+	Reference = "-arx",
+	Definition = "-arx"
+}
 
 let treeViewProvider : TreeViewProvider;
 let gtagsRoot : String | undefined;
@@ -37,16 +43,25 @@ export class TreeItemNode extends vscode.TreeItem {
         ]
     }
     
-    // iconPath： 为该项的图标因为我们是通过上面的 Map 获取的，所以我额外写了一个方法，放在下面
-    // iconPath = TreeItemNode.getIconUriForLabel(this.label);
+}
+export class DefinitionProvider implements vscode.DefinitionProvider{
+	provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): 
+	vscode.ProviderResult<vscode.Definition | vscode.DefinitionLink[]>{
+		let range = document.getWordRangeAtPosition(position)
+		let word = document.getText(range);
+		let locations = FindGtagsByWord(word, GtagsFindType.Definition);
+		return locations;
+	}
+}
 
-    // __filename：当前文件的路径
-    // 重点讲解 Uri.file(join(__filename,'..', '..') 算是一种固定写法
-    // Uri.file(join(__filename,'..','assert', ITEM_ICON_MAP.get(label)+''));   写成这样图标出不来
-    // 所以小伙伴们就以下面这种写法编写
-    // static getIconUriForLabel(label: string):Uri {
-    //     return Uri.file(join(__filename,'..', '..' ,'src' ,'assert', ITEM_ICON_MAP.get(label)+''));
-    // }
+export class ReferenceProvider implements vscode.ReferenceProvider{
+	provideReferences(document: vscode.TextDocument, position: vscode.Position, context: vscode.ReferenceContext, token: vscode.CancellationToken): 
+	vscode.ProviderResult<vscode.Location[]>{
+		let range = document.getWordRangeAtPosition(position)
+		let word = document.getText(range);
+		let locations = FindGtagsByWord(word, GtagsFindType.Reference);
+		return locations;
+	}
 }
 
 export class TreeViewProvider implements vscode.TreeDataProvider<TreeItemNode>{
@@ -94,54 +109,55 @@ function FindGtagsRoot():string | undefined{
 	} while (wordPath_last != wordPath);
 	return undefined;
 }
+
+function FindGtagsByWord(word:string, ftype : GtagsFindType):vscode.Location[] | undefined{
+	let locations : vscode.Location[] = [];
+	var cmdStr = "D:\\glo663wb\\bin\\global.exe " + ftype + " " + word;
+	let wordPath = (<vscode.WorkspaceFolder[]>vscode.workspace.workspaceFolders)[0].uri.fsPath;
+	console.log(cmdStr);
+	console.log(wordPath);
+	let exeOpt : ExecSyncOptionsWithStringEncoding;
+	exeOpt =  {cwd : wordPath, encoding: "utf8"};
+	let stdout = execSync(cmdStr,exeOpt);
+	const regex = /^(\w+)\s+(\d+)\s(\S+)\s(.+)$/gm;
+	// console.log(stdout);
+	let findResult = regex.exec(stdout);
+	while (findResult !== null) {
+		// This is necessary to avoid infinite loops with zero-width matches
+		if (findResult.index === regex.lastIndex) {
+			regex.lastIndex++;
+		}
+		let pos = new vscode.Position(Number(findResult[2]) - 1, 0);
+		let loction = new vscode.Location(vscode.Uri.file(findResult[3]), new vscode.Range(pos, pos));
+		locations.push(loction);
+		console.log(findResult[3]);
+		console.log(basename(loction.uri.toString()));
+		console.log(loction.range);
+		findResult = regex.exec(stdout);
+	}
+	if(locations.length == 0) return undefined;
+	return locations;
+}
+
 function FindDefineAndRefer(uri?: vscode.Uri, position?: vscode.Position){
 	if (vscode.window.activeTextEditor) {
 		// take args from active editor
 		let editor = vscode.window.activeTextEditor;
 		let range = editor.document.getWordRangeAtPosition(editor.selection.active)
-		let locations : vscode.Location[] = [];
-		if(range && vscode.workspace.workspaceFolders){
-			let word = editor.document.getText(range);
-			var exec = require('child_process').exec; 
-			var cmdStr = "D:\\glo663wb\\bin\\global.exe -ax " + word;
-			let wordPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-			console.log(cmdStr);
-			console.log(wordPath);
-			exec(cmdStr, {cwd : wordPath}, function(err : string,stdout : string, stderr : string){
-				if(err) {
-					console.log('get weather api error:'+stderr);
-				} else {
-					const regex = /^(\w+)\s+(\d+)\s(\S+)\s(.+)$/gm;
-					// console.log(stdout);
-					let findResult = regex.exec(stdout);
-					while (findResult !== null) {
-						// This is necessary to avoid infinite loops with zero-width matches
-						if (findResult.index === regex.lastIndex) {
-							regex.lastIndex++;
-						}
-						let pos = new vscode.Position(Number(findResult[2]), 0);
-						let loction = new vscode.Location(vscode.Uri.file(findResult[3]), new vscode.Range(pos, pos));
-						locations.push(loction);
-						console.log(findResult[3]);
-						console.log(basename(loction.uri.toString()));
-						console.log(loction.range);
-						findResult = regex.exec(stdout);
-					}
-					if(locations.length == 1){
-						
-						const defaults: vscode.TextDocumentShowOptions = {
-							selection : locations[0].range
-						};
-						let edtior = vscode.window.showTextDocument(locations[0].uri, defaults);
-					}
-
-					treeViewProvider.setData(locations);
-					vscode.commands.executeCommand(`gtags-result-item.focus`);
-				}
-			});
-		} else {
-			vscode.window.showInformationMessage("未识别到单词");
+		let word = editor.document.getText(range);
+		let locations = FindGtagsByWord(word, GtagsFindType.Definition);
+		if(!locations){
+			vscode.window.showErrorMessage("未找到\"" + word + "\"的任何定义");
+			return;
 		}
+		if(locations.length == 1){
+			const defaults: vscode.TextDocumentShowOptions = {
+				selection : locations[0].range
+			};
+			let edtior = vscode.window.showTextDocument(locations[0].uri, defaults);
+		}
+		treeViewProvider.setData(locations);
+		vscode.commands.executeCommand(`gtags-result-item.focus`);
 	}
 }
 
@@ -153,26 +169,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "gtagsc" is now active!');
 	gtagsRoot = FindGtagsRoot();
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable,
-		vscode.commands.registerCommand('gtagsc.FindDefineAndRefer', FindDefineAndRefer)
+	let defineProcider = new DefinitionProvider();
+	let referenceProvider = new ReferenceProvider();
+	context.subscriptions.push(
+		vscode.commands.registerCommand('gtagsc.FindDefineAndRefer', FindDefineAndRefer),
+		vscode.languages.registerDefinitionProvider(["c","h","cpp"], defineProcider),
+		vscode.languages.registerReferenceProvider(["c","h","cpp"], referenceProvider)
 		);
-	context.subscriptions.push(disposable,
+	
+	context.subscriptions.push(
 		vscode.commands.registerCommand('itemClick', (loc:vscode.Location) => {
 			const defaults: vscode.TextDocumentShowOptions = {
 				selection : loc.range
 			};
 			vscode.window.showTextDocument(loc.uri, defaults);
 		}));
+
 	// 实例化 TreeViewProvider
 	treeViewProvider = new TreeViewProvider();
 	
